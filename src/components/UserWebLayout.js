06d969302fa81481b545f342bfba8b83fa5879be
ABU DAVID <65656238+davidabuu@users.noreply.github.com>
2023-01-14 02:23:04 +0100

    UI Better

--- src/components/UserWebLayout.js
+++ src/components/UserWebLayout.js
@@ -3,7 +3,7 @@ const UserWebLayout = ({ webtitle, children }) => {
   return (
     <>
       <Head>
-        <title>{webtitle} | Zamps</title>
+        <title>{webtitle} | EKOM</title>
       </Head>
       {children}
     </>
